f7b94c6db265ceb337d75519081e28989a76110c
Sol Kim <solkim813@gmail.com>
2017-01-31 19:14:21 -0500

    populate site automatically

---
+++ site/js/js.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	var gifcount = 1;
+
+	// add rows to display
+	for (var i = 1; i <= 100; i += 4) {
+		var nomoregifs = false;
+
+		// create row
+		var row = '<div class="row">';
+		for (var j = i; j < i + 4; j++) {
+			if (j <= gifcount) {
+				row += '<div class="col-sm-3"><img src="' + j + '/gif.gif"></div>';
+			} else {
+				row += '<div class="col-sm-3"></div>';
+				nomoregifs = true;
+			}
+		}
+		row += '</div>';
+
+		// add row to container
+		$("#gifs").append(row);
+
+		// break if no more gifs to display
+		if (nomoregifs) {
+			break;
+		}
+	}
+});
